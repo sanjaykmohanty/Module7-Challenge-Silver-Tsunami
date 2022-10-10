4b67f79e847e7b3733275d10e8366b8d6ec4f27e
sanjaykmohanty <sanj.moha@gmail.com>
2022-10-10 00:09:11 -0400

    adding summary changes

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -36,3 +36,29 @@ ON E.emp_no = T.emp_no
 AND DE.to_date = '9999-01-01'
 AND E.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER By E.emp_no;
+
+-- Create Roles to Fill table to store department information for each employee 
+SELECT DISTINCT ON (RT.emp_no) RT.emp_no, RT.first_name, RT.last_name, RT.title, D.dept_name
+INTO roles_to_fill
+FROM retirement_titles AS RT 
+JOIN dept_emp DE
+ON (DE.emp_no = RT.emp_no)
+JOIN departments D
+ON (D.dept_no = DE.dept_no)
+WHERE RT.to_date = '9999-01-01'
+ORDER BY RT.emp_no, title, dept_name;
+
+-- Create Roles to Fill Summary table to store retiring roles and count of retiring roles for each department 
+SELECT RF.dept_name, RF.title, COUNT(RF.title) AS title_count 
+INTO roles_to_fill_summary
+FROM (SELECT title, dept_name from roles_to_fill) as RF
+GROUP BY RF.dept_name, RF.title
+ORDER BY RF.dept_name, RF.title, title_count;
+
+-- Create Mentor by Department table to store potential mentor count by department 
+SELECT RF.dept_name, RF.title, COUNT(RF.title) 
+INTO mentor_by_dept
+FROM (SELECT title, dept_name from roles_to_fill) as RF
+WHERE RF.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY RF.dept_name, RF.title
+ORDER BY RF.dept_name, RF.title, count;
